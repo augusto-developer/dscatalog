b4130e040ea66558de4b31dd8812eec76da7d669
LopezTw <caiohg_94@hotmail.com>
2023-10-24 20:05:45 -0300

    findById using Optional

---
+++ backend/src/main/java/com/augustodeveloper/dscatalog/dto/CategoryDTO.java
@@ -0,0 +1,31 @@
+package com.augustodeveloper.dscatalog.dto;
+
+import com.augustodeveloper.dscatalog.entities.Category;
+
+public class CategoryDTO {
+	
+	private Long id;
+	private String name;
+	
+	public CategoryDTO() {}
+	
+	public CategoryDTO(Long id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+	
+	public CategoryDTO(Category entity) {
+		id = entity.getId();
+		name = entity.getName();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+	
+}
--- backend/src/main/java/com/augustodeveloper/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/augustodeveloper/dscatalog/services/CategoryService.java
@@ -1,21 +1,34 @@
 package com.augustodeveloper.dscatalog.services;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.augustodeveloper.dscatalog.dto.CategoryDTO;
 import com.augustodeveloper.dscatalog.entities.Category;
 import com.augustodeveloper.dscatalog.repositories.CategoryRepository;
 
 @Service
 public class CategoryService {
 	
+	@Autowired
 	private CategoryRepository rep;
 	
 	@Transactional(readOnly = true)
-	public List<Category> findAll(){
-		return rep.findAll();
+	public List<CategoryDTO> findAll(){
+		List<Category> list = rep.findAll();
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+	}
+	
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id){
+		Optional<Category> obj = rep.findById(id);
+		Category entity = obj.get();
+		return new CategoryDTO(entity);
 	}
 	
 }
